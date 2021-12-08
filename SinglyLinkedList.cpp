88f985c1a19aa12441f644bc5e6408f9f5448f73
Ayesha Sahar <83140034+AyeshaSahar@users.noreply.github.com>
2021-12-08 18:16:24 +0500

    Add files via upload

---
+++ SinglyLinkedList.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+   int data;
+   struct Node *next;
+};
+
+struct Node* head = NULL;
+
+void insert(int new_data) {
+   struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+   new_node->data = new_data;
+   new_node->next = head;
+   head = new_node;
+}
+
+void display() {
+   struct Node* ptr;
+   ptr = head;
+   while (ptr != NULL) {
+      cout<< ptr->data <<" ";
+      ptr = ptr->next;
+   }
+}
+
+int main() {
+   insert(1);
+   insert(3);
+   insert(2);
+   insert(2);
+   insert(7);
+
+   cout<<"The linked list is: ";
+   display();
+   return 0;
+}
+
